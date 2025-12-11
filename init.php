864555ab7b1e569f235a474c860cf840addf832c
2DAW <github.goatskin743@passinbox.com>
2025-12-11 11:26:04 +0100

    configurando el archivo init.php

--- init.php
+++ init.php
@@ -7,7 +7,7 @@ date_default_timezone_set('America/Sao_Paulo');
 // conexão com o banco de dados
 
 define('BD_SERVIDOR','localhost');
-define('BD_USUARIO','root');
-define('BD_SENHA','');
+define('BD_USUARIO','myexamen209c');
+define('BD_SENHA','Juanjose2003@');
 define('BD_BANCO','livraria');
 
